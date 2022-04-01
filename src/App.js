b6be9ccb0a26477f60cc25735a6e5589b006e24d
Kaitlyn Zhen <kzhen7923@gmail.com>
2022-04-01 09:53:34 -0400

    added api and credit/debit state

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
 import LogIn from './components/Login';
+import axios from 'axios';
 
 class App extends Component {
   constructor() {  // Create and initialize state
@@ -14,9 +15,45 @@ class App extends Component {
       currentUser: {
         userName: 'Joe Smith',
         memberSince: '07/23/96',
-      }
+      },
+      credits: [],
+      debits:[]
     }
   }
+
+  async componentDidMount() {
+    let CreditAPILink = 'https://moj-api.herokuapp.com/credits';  
+
+    try {  
+      let response = await axios.get(CreditAPILink);
+      console.log(response.data);  
+      // To get data object in the response, need to use "response.data"
+      this.setState({credits: response.data});  // Store received data in state's "users" object
+    } 
+    catch (error) {  // Print out errors at console when there is an error response
+      if (error.response) {
+        // The request was made, and the server responded with error message and status code.
+        console.log(error.response.data);  // Print out error message (e.g., Not Found)
+        console.log(error.response.status);  // Print out error status code (e.g., 404)
+      }    
+    }
+
+    let DebitAPILink = 'https://moj-api.herokuapp.com/debits';  
+
+    try {  
+      let response = await axios.get(DebitAPILink);
+      console.log(response.data);  
+      // To get data object in the response, need to use "response.data"
+      this.setState({debits: response.data});  // Store received data in state's "users" object
+    } 
+    catch (error) {  // Print out errors at console when there is an error response
+      if (error.response) {
+        // The request was made, and the server responded with error message and status code.
+        console.log(error.response.data);  // Print out error message (e.g., Not Found)
+        console.log(error.response.status);  // Print out error status code (e.g., 404)
+      }    
+    }
+  }  
 
   // Update state's currentUser (userName) after "Log In" button is clicked
   mockLogIn = (logInInfo) => {  
