a00aa4008805f16108c5a5391dd12349a7c591a8
Kaitlyn Zhen <kzhen7923@gmail.com>
2022-04-01 19:50:57 -0400

    user input adds to debit list

--- src/App.js
+++ src/App.js
@@ -12,32 +12,34 @@ class App extends Component {
   constructor() {  // Create and initialize state
     super(); 
     this.state = {
-      accountBalance: 14568.27,
+      accountBalance: 1500.24,
       currentUser: {
         userName: 'Joe Smith',
         memberSince: '07/23/96',
       },
       credits: [],
-      debits:[]
+      debits:[],
+      totalCredit:1000,
+      totelDebit:10
     }
   }
 
   async componentDidMount() {
-    let CreditAPILink = 'https://moj-api.herokuapp.com/credits';  
+    // let CreditAPILink = 'https://moj-api.herokuapp.com/credits';  
 
-    try {  
-      let response = await axios.get(CreditAPILink);
-      console.log(response.data);  
-      // To get data object in the response, need to use "response.data"
-      this.setState({credits: response.data});  // Store received data in state's "users" object
-    } 
-    catch (error) {  // Print out errors at console when there is an error response
-      if (error.response) {
-        // The request was made, and the server responded with error message and status code.
-        console.log(error.response.data);  // Print out error message (e.g., Not Found)
-        console.log(error.response.status);  // Print out error status code (e.g., 404)
-      }    
-    }
+    // try {  
+    //   let response = await axios.get(CreditAPILink);
+    //   console.log(response.data);  
+    //   // To get data object in the response, need to use "response.data"
+    //   this.setState({credits: response.data});  // Store received data in state's "users" object
+    // } 
+    // catch (error) {  // Print out errors at console when there is an error response
+    //   if (error.response) {
+    //     // The request was made, and the server responded with error message and status code.
+    //     console.log(error.response.data);  // Print out error message (e.g., Not Found)
+    //     console.log(error.response.status);  // Print out error status code (e.g., 404)
+    //   }    
+    // }
 
     let DebitAPILink = 'https://moj-api.herokuapp.com/debits';  
 
@@ -63,6 +65,22 @@ class App extends Component {
     this.setState({currentUser: newUser})
   }
 
+  addDebit = (description,amount,date) => {
+    // console.log("add descriptionription");
+    // console.log(description);
+    // console.log("add debit:");
+    // console.log(amount);
+    const newBalance = this.state.accountBalance + amount;
+    this.setState({accountBalance: newBalance});
+    // console.log(newBalance);
+    // console.log({amount,description,date});
+    let id = description;
+    let entry = {id,amount,description,date}
+    console.log(entry);
+    this.state.debits.push(entry);
+    
+  }
+
   // Create Routes and React elements to be rendered using React components
   render() {  
     const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
@@ -70,7 +88,7 @@ class App extends Component {
         <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}  />
     );
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />);  // Pass props to "LogIn" component
-    const DebitsComponent = () => (<Debits debits={this.state.debits} />);  //pass debits array
+    const DebitsComponent = () => (<Debits debits={this.state.debits} addDebit={this.addDebit} accountBalance={this.state.accountBalance}/>);  //pass debits array
     return (
         <Router>
           <div>
--- src/components/Debits.js
+++ src/components/Debits.js
@@ -1,24 +1,61 @@
 // src/components/Debits.js
+import React, { Component } from 'react'
+import AccountBalance from './AccountBalance';
+import {Link} from 'react-router-dom';
 
-const Debits = (props) => {
-	let debitsView = () => {
-        const { debits } = props;
-        return debits.map((debit) => {
-            let date = debit.date.slice(0,10);
-            return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
-        }) 
-    }
+class Debits extends Component {
+  constructor () {  // Create and initialize state
+    super()
+    
+
+    
+  }
+
+  debitsView = () => {
+    const { debits } = this.props;
+    return debits.map((debit) => {
+        let date = debit.date.slice(0,10);
+        return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
+    }) 
+  } 
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.description.value);
+    console.log(e.target.amount.value);
+    const current = new Date();
+    //console.log(current.getDate());
+    //console.log(current.getMonth());
+    //console.log(current.getFullYear());
+    //const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+    const date = `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()}`;
+    //console.log(date);
+    this.props.addDebit(e.target.description.value,e.target.amount.value,date);
+    
+    
+    
+  }
+  render() {
     return (
-    	<div>
-    	   <h1>Debits</h1>
-    	   {debitsView()}
-           <form onSubmit={props.addDebit}>
-             <input type="text" name="description" />
-             <input type="number" name="amount" />
-             <button type="submit">Add Debit</button>
-           </form>
-    	</div>
+      <div>
+        <h1>Debits</h1>
+        {this.debitsView()}
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" name="description" placeholder='description' />
+            <input type="number" name="amount" placeholder='amount' />
+            <button type="submit">Add Debit</button>
+          </form>
 
-    )
+          <AccountBalance accountBalance={this.props.accountBalance}/>
+          <Link to="/">Return to Home</Link>
+
+          
+        
+      </div>
+
+    )    
+        
+  }
 }
+
 export default Debits;
