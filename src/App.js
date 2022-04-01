bcd83208b118d1c2a64010cfb8994023535fd66b
Kaitlyn Zhen <kzhen7923@gmail.com>
2022-04-01 10:18:37 -0400

    home links debit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
 import LogIn from './components/Login';
+import Debits from './components/Debits';
 import axios from 'axios';
 
 class App extends Component {
