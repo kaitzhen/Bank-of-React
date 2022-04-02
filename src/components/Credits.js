53213bcc07d06196bb5e1c5505694e4cf1d60c1d
Kaitlyn Zhen <kzhen7923@gmail.com>
2022-04-01 20:10:30 -0400

    created credit component

---
+++ src/components/Credits.js
@@ -0,0 +1,24 @@
+// src/components/Credits.js
+
+const Credits = (props) => {
+	let creditsView = () => {
+    const { credits } = props;
+    return credits.map((credit) => {
+      let date = credit.date.slice(0,10);
+      return <li key={credit.id}>{credit.amount} {credit.description} {date}</li>
+    }) 
+  }
+  return (
+    <div>
+      <h1>Credits</h1>
+      {creditsView()}
+      <form onSubmit={props.addCredit}>
+        <input type="text" name="description" />
+        <input type="number" name="amount" />
+        <button type="submit">Add Credit</button>
+      </form>
+    </div>
+  )
+}
+
+export default Credits;
